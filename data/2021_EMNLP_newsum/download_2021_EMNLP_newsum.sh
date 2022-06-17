52a460ce9e8dd702954849598e7ba0c42a853e81
nicolasspring <springnicolas@hispeed.ch>
2022-06-17 10:52:45 +0200

    update repo for simple wget download

--- data/2021_EMNLP_newsum/download_2021_EMNLP_newsum.sh
+++ data/2021_EMNLP_newsum/download_2021_EMNLP_newsum.sh
@@ -6,15 +6,11 @@ script_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 || exit 1; pwd -P )"
 
 bucket_name="20min-simplified-language-uzh"
 object_key="EMNLP_newsum_2021_A_New_Dataset_TS_DE.zip"
+download_url="https://${bucket_name}.s3.eu-central-1.amazonaws.com/${object_key}"
 outfile="$script_path/$object_key"
 outdir="${outfile%*.zip}"
 
-
-python3 download_s3_object.py \
-    --bucket-name "$bucket_name" \
-    --object-key "$object_key" \
-    --output-file "$outfile"
-
+wget -O "$outfile" "$download_url"
 
 mkdir -p "$outdir"
 unzip -q "$outfile" -d "$outdir"
